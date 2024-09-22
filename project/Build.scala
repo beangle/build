de536c4210dbca7f9f16b30fff2720287d105c81
段体华 <duantihua@gmail.com>
2024-09-22 10:47:19 +0800

    merge modules

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,12 @@ import sbt.Keys._
 import sbt._
 
 object BuildSettings {
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
 
   val commonSettings = Seq(
     organizationName := "The Beangle Software",
     startYear := Some(2005),
-    licenses += ("GNU General Public License version 3", new URL("http://www.gnu.org/licenses/lgpl-3.0.txt")),
+    licenses += ("GNU General Public License version 3", url("http://www.gnu.org/licenses/lgpl-3.0.txt")),
     libraryDependencies ++= Seq(scalaTest % Test),
     crossPaths := true,
 
